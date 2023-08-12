f4f1c71f26a4300101954a3b907a5b91dace2653
Farshad <asadpoor.f@gmail.com>
2023-08-13 00:01:31 +0330

    connect to zoho first action

--- zohoapp/views.py
+++ zohoapp/views.py
@@ -11,12 +11,6 @@ class ApiZoho(APIView):
         serializer = serilizers.ZohoSerializer(data=request.data)
         if serializer.is_valid():
             r_data = serializer.validated_data
-            # print(r_data["name"])
-            # print(r_data["channel"])
-            # print(r_data["key"])
             requrl = f"https://cliq.zoho.com/company/767068114/api/v2/bots/{r_data['channel']}/incoming?zapikey={r_data['key']}"
-
-            g = requests.post(url=requrl ,data=json.dumps({"service": r_data["name"]}), headers={'Content-Type': 'application/json'})
-            print(g)
-
+            requests.post(url=requrl ,data=json.dumps({"service": r_data["name"]}), headers={'Content-Type': 'application/json'})
             return Response(data={"status": r_data}, status=status.HTTP_200_OK)
