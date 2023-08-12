d020c5c6798c1560fd705208022b7d87ea94d3a0
Farshad <asadpoor.f@gmail.com>
2023-08-12 22:17:57 +0330

    change api az rest and added the serializer

--- zohoapp/views.py
+++ zohoapp/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from . import serilizers
 class ApiZoho(APIView):
     def post(self,request):
-        return Response(data={"status":"ok"},status=status.HTTP_200_OK)
+        serializer = serilizers.ZohoSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+        return Response(data={"status":data},status=status.HTTP_200_OK)
